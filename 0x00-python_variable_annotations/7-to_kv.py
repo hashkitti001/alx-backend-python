85d6fa0fe2e4f7a78398131716caae21fd5c1851
hashkitti001 <gidemho@gmail.com>
2024-10-11 16:34:51 +0100

    hot fix to 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,6 +4,6 @@ from typing import Union, Tuple
 import math
 
 
-def to_kv(k: str, v: Union[int, float]) -> Union[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''Returns a tuple containing a number and it's square'''
     return (k, float(v ** 2))
