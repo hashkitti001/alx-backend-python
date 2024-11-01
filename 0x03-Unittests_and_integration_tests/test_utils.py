69371a977e24eb7cb3dc66ce13f2b23e43c9337c
hashkitti001 <iJeddy5.0@gmail.com>
2024-11-01 13:09:31 +0100

    add tests for mock http calls with unittest.Mock

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 """Tests module for the utils module."""
 from typing import Dict, Tuple, Union
-from unittest import TestCase
+import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 from utils import (
-    access_nested_map
+    access_nested_map,
+    get_json
 )
 
 
-class TestAccessNestedMap(TestCase):
+class TestAccessNestedMap(unittest.TestCase):
     """Tests the access_nested_map function."""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -37,3 +39,23 @@ class TestAccessNestedMap(TestCase):
         """Tests if a KeyError is raised for invalid key entries."""
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Tests the get_json function."""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(
+        self,
+        test_url: str,
+        test_payload: Dict,
+    ) -> None:
+        """Tests get_json's output"""
+        attrs = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+
+# class TestMemoize(unittest.TestCase):
+#     """Tests the memoize """
